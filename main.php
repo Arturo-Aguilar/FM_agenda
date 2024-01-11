7f118b83d5ddab4095295892303b511f4143d2d8
Arturo <morenoaguilar2904@gmail.com>
2024-01-11 11:32:11 -0600

    Se crea formulario para iniciar sesion

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset='utf-8' />
+  <script src='fullcalendar/packages/core/index.global.js'></script>
+  <script src='fullcalendar/packages/core/locales/es.global.js'></script>
+  <script src='fullcalendar/dist/index.global.min.js'></script>
+  <script src="script.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <!--CONTENEDOR DEL CALENDARIO-->
+  <div id='calendar'></div>
+ </body>
+</html>

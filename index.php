7248d8f5c97a751e2bfecbd014b1484fcf3e5317
Arturo <morenoaguilar2904@gmail.com>
2024-01-16 17:27:40 -0600

    se agrego margenes a imagen de portada

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulario CSS</title>
+    <title>AGENDA FABRICA MARIA</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
